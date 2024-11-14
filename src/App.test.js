c2cff1a066b5744071ee4d7ce56b23c36d41075b
SHASWAT PRAKASH <shaswatp@snapwork.com>
2024-11-15 03:02:24 +0530

    FifthforTest

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Music Albums (2020-2024)/i);
+  const linkElement = screen.getByText(/Music Albums (2020-2024)/);
   expect(linkElement).toBeInTheDocument();
 });
